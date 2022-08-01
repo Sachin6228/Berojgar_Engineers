757c76fa0e505e65c51c6f6e38ef7f526e3cbc77
admin <admin@DESKTOP-QSK7NRS>
2022-08-01 22:08:37 +0530

    [Sachin] 1st August --> 2 Classes --> 1) PushProject02 2) PullProject01

---
+++ src/be01/PushProject01.java
@@ -0,0 +1,11 @@
+package be01;
+
+public class PushProject01 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("This needs to be push to the GitHub");
+	}
+}
---
+++ src/be02/PushProject02.java
@@ -0,0 +1,12 @@
+package be02;
+
+public class PushProject02 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("This needs to be pulled from GitHub");
+		
+	}
+}
