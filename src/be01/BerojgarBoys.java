babeb9e8c56ddfbe7a5d57b8b8afa11b9cd21b8c
HP <HP@DESKTOP-V5J41QF>
2022-08-02 11:49:22 +0530

    [Vishal]-->Thank You

---
+++ src/be01/BerojgarBoys.java
@@ -0,0 +1,10 @@
+package be01;
+
+public class BerojgarBoys {
+
+	public static void main(String[] args) {
+
+		System.out.println("Thanks for supporting");
+	}
+
+}
---
+++ src/be02/Demo4.java
@@ -0,0 +1,11 @@
+package be02;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Demo for block");
+	}
+
+}
