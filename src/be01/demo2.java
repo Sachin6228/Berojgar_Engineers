1fbef9dbe85c06bb17e3315d3a65e99479453bbd
sanke <sanke@sanket>
2022-08-02 10:58:21 +0530

    [sanket's code] demo1, demo2

---
+++ src/be01/demo2.java
@@ -0,0 +1,11 @@
+package be01;
+
+public class demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("always a simp for dhanashree ma'am");
+		
+	}
+
+}
---
+++ src/be02/demo1.java
@@ -0,0 +1,11 @@
+package be02;
+
+public class demo1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("har har mahadev");
+		
+	}
+
+}
